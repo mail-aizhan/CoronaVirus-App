09ad85880295d2d9c25ffe3ed9881062e5fd788f
Aizhan Zhamanakova <mail.aizhan@gmail.com>
2021-01-07 01:53:11 +0300

    first commit

---
+++ src/app/services/corona.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoronaService {
+
+  constructor(private http: HttpClient) { }
+  //method  make request to and it returns observable
+  getCountries():Observable<any>{
+    const url =" https://api.covid19api.com/countries"
+    return this.http.get<any>(url)
+  }
+  getCoronaRealtimeData(country):Observable<any>{
+    const url ="http://api.covid19api.com/total/dayone/country/"+ country 
+    return this.http.get<any>(url)
+    
+  }
+}
